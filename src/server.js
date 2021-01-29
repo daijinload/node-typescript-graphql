bb6db74ef26e08c39d279624fad9230fd7722f60
daijinload <daijinload@gmail.com>
2021-01-29 22:52:18 +0900

    [add] graphqlを複数指定してみた (#3)

--- src/server.js
+++ src/server.js
@@ -4,13 +4,15 @@ var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 var schema = buildSchema(`
   type Query {
+    hello: String
     hello2(name: String): String
   }
 `);
 var root = {
+    hello: () => 'Hello world!',
     hello2: (hello) => {
         console.log(hello);
-        return 'world name=' + hello.name;
+        return 'world name ' + hello.name;
     }
 };
 var app = express();
--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,23 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-//hello: String,
-// hello: () => 'Hello world!',
-
+// オブジェクトで渡されるので、特にinterfaceを使う必要は無いんだけど、せっかくなのでTSCっぽく使ってみた。
 interface Ihello {
   name: string
 }
 
 var schema = buildSchema(`
   type Query {
+    hello: String
     hello2(name: String): String
   }
 `);
  
 var root = { 
+  hello: () => 'Hello world!',
   hello2: (hello: Ihello):string => {
     console.log(hello)
-    return 'world name=' + hello.name;
+    return 'world name ' + hello.name;
   }
 };
  
